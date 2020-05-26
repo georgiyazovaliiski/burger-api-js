1c271ede7ab5a288d5cd689f785e12d1213d4adf
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2020-05-26 12:33:00 +0300

    DB uri fix

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,6 +1,7 @@
 const mongodb = require("mongoose")
 
-const dbUrl = "mongodb://127.0.0.1:27017/burger-api"
+const dbUrl = "mongodb://heroku_p9k6qfp4:mongo123@ds131510.mlab.com:31510/heroku_p9k6qfp4"
+//const dbUrl = "mongodb://127.0.0.1:27017/burger-api"
 
 mongodb.connect(dbUrl,{
     useNewUrlParser:true,
